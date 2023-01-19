f9d9b0e99902663f0de4aa5bf9a8d609cd57ab70
Odiaka Emmanuella Anita <anitaudrey@gmail.com>
2023-01-19 13:09:03 +0100

    Creating component temp0lates for our chat finctionalities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed.jsx'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         projectID="9daccde9-f667-4834-a5c3-7529c990707b"
         userName="Anita"
         userSecret="123456"
+        renderChatFeed = {(chatAppProps) => <ChatFeed  {...chatAppProps} />}
+        onNewMessage = { () => new Audio('https://chat-engine-assets.s3.amazonawm.com/click.mp3')}
       />
     </div>
   );
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,10 @@
+const ChatFeed = (props) => {
+    console.log(props)
+    return (
+        <div className="div">
+            chat feed
+        </div>
+    )
+}
+
+export default ChatFeed
